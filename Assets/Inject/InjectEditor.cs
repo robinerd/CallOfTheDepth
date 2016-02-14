cea0b784e5d68e4b16bd609edc5c8f5beea3d48a
Robinerd <robinerd@gmail.com>
2016-02-14 01:09:10 +0100

    Forgot two files

---
+++ Assets/Inject/InjectEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.Collections;
+
+#if UNITY_EDITOR
+public class InjectEditor : EditorWindow {
+
+    [MenuItem("Inject/Inject Script Dependencies")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        foreach(BaseBehaviour obj in GameObject.FindObjectsOfType<BaseBehaviour>())
+        {
+            obj.InjectDependencies(true, true);
+            EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+        }
+    }
+}
+#endif
